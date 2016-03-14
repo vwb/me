b7d724c042a51a07e01ba709dee9458683c8e872
Vincent Budrovich <vwb@berkeley.edu>
2016-03-14 15:12:11 -0700

    Initial Commit

---
+++ personal_site.js
@@ -0,0 +1,45 @@
+var bio_button = document.getElementById("bio");
+var proj_button = document.getElementById("proj");
+var contact_button = document.getElementById("contact");
+var project = document.getElementById("project-info");
+var bio = document.getElementById("bio-info");
+var contact_body = document.getElementById("contact-info")
+
+document.addEventListener("click", function(event){
+	if (event.target.id === "bio" || event.target.id === "proj" || event.target.id === "contact"){
+		toggle(event.target.id);
+	}
+})
+
+var toggle = function(id){
+
+	if (id === "bio"){
+
+		bio.className = "description center fade-in"
+		project.className += " hidden"
+		contact_body.className += " hidden"
+		bio_button.className = "tag hvr-underline-reveal selected"
+		proj_button.className = "tag hvr-underline-reveal unselected"
+		contact_button.className = "tag hvr-underline-reveal unselected"
+
+	} else if (id === "proj") {
+
+		bio.className += " hidden"
+		contact_body.className += " hidden"
+		project.className = "projects group fade-in" 
+		proj_button.className = "tag hvr-underline-reveal selected"
+		bio_button.className = "tag hvr-underline-reveal unselected"
+		contact_button.className = "tag hvr-underline-reveal unselected"
+
+	} else {
+
+		contact_body.className = "description center fade-in"
+		project.className += " hidden"
+		bio.className += " hidden"
+		bio_button.className = "tag hvr-underline-reveal unselected"
+		proj_button.className = "tag hvr-underline-reveal unselected"
+		contact_button.className = "tag hvr-underline-reveal selected"
+
+	}
+
+}
